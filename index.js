d862d31cc1be916269a4306c9fa1ac3afc31370a
Devaldo <devaldo.nas@gmail.com>
2023-11-18 11:25:49 -0200

    primeiro commit - entrega de projeto

---
+++ index.js
@@ -0,0 +1,29 @@
+//Classificador de Nível de Herói
+
+let heroi = "Super Dev"
+
+let quantidadeXp = 90000
+
+if(quantidadeXp <= 1000){
+console.log(" O Herói de nome "+heroi + " está no nível de " +"Ferro ")}
+
+else if(quantidadeXp >= 1001) if(quantidadeXp <= 2000){
+console.log(" O Herói de nome "+heroi + " está no nível de " +"Bronze ")}
+
+else if(quantidadeXp >= 2001) if(quantidadeXp <= 5000){
+console.log(" O Herói de nome "+heroi + " está no nível de " +"Prata ")}
+
+else if(quantidadeXp >= 5001) if(quantidadeXp <= 7000){
+console.log(" O Herói de nome "+heroi + " está no nível de " +"Ouro ")}
+
+else if(quantidadeXp >= 7001) if(quantidadeXp <= 8000){
+console.log(" O Herói de nome "+heroi + " está no nível de " +"Platina ")}
+
+else if(quantidadeXp >= 8001) if(quantidadeXp <= 9000){
+console.log(" O heroi de nome "+heroi + " está no nível de " +"Ascendente ")}
+
+else if(quantidadeXp >= 9001) if(quantidadeXp <= 10000){
+console.log(" O Herói de nome "+heroi + " está no nível de " +"Imortal ")}
+
+else if(quantidadeXp >= 10001){
+console.log(" O Herói de nome "+heroi + " está no nível de " +"Radiante ")}
